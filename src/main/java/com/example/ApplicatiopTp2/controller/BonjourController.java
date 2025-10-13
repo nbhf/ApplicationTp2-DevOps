c37dfcfff3bf0282490ea8bef1f76f044b9a457a
nbhf <houdabhf123@gmail.com>
2025-10-13 12:56:05 +0100

    Initial commit - Application Spring Boot avec Dockerfile

---
+++ src/main/java/com/example/ApplicatiopTp2/controller/BonjourController.java
@@ -0,0 +1,14 @@
+package com.example.ApplicatiopTp2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/monapi/v1")
+public class BonjourController {
+    @GetMapping("/bonjour")
+    public String saluer(){
+        return "Bonjour";
+    }
+}
---
+++ src/test/java/com/example/ApplicatiopTp2/ApplicatiopTp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApplicatiopTp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicatiopTp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
